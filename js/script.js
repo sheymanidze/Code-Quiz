4fae88b3dbc45b5f25a0a1517dec95cc527b74ce
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-03-09 13:00:49 -0500

    js start

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var startBtn = document.querySelector("#start");
+
+//timer
+var time = document.querySelector("timer");
+
+//questions
+var questionNumber = document.querySelector("questionNumber");
+var questionText = document.querySelector("#questionText");
+
+//choices
+var option1 = document.querySelector("option1");
+var option2 = document.querySelector("option2");
+var option3 = document.querySelector("option3");
+var option4 = document.querySelector("option4");
+
+//correct/wrong answers
+var correct = document.querySelector("correct");
+var wrong = document.querySelector("wrong");
+
+//Results
+var points = document.querySelector("points");
+
+var option = document.querySelector("option")
